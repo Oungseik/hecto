585be0030626e2f20e29ada90945ae8551b9520b
Oungseik <mhemaungthuwin@gmail.com>
2021-10-17 18:04:27 +0630

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hecto
+
+A text editor in rust learning from Philipp Flenker [Hecto: Build your own text editor in Rust
+](https://www.philippflenker.com/hecto/)
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io::{self, Read};
+
+fn main() {
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+
+        print!("{}", c);
+        if c == 'q' {
+            break;  
+        }
+    }
+}
